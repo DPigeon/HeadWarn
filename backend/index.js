2da382444ab8fe13241fc84593e310d0650f8519
DPigeon <D_Pig@encs.concordia.ca>
2019-09-28 18:10:29 -0400

    Edits on the backend

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello lementAI best team, continue comme ça!')
+app.get("/", (req, res) => {
+  res.send(
+    "Hello ImplementAI best team, continue comme ça! This is the backend!"
+  );
 });
 
 app.listen(8000, () => {
-    console.log('Example app listening on port 8000!')
+  console.log("Example app listening on port 8000!");
 });
