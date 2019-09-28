26f1baac709e3d1efbbd82653856ac3f0e23b7f1
eglen <eglen@live.com>
2019-09-28 15:21:52 -0400

    Set up NodeJS  with Express

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello lementAI best team, continue comme ça!')
+});
+
+app.listen(8000, () => {
+    console.log('Example app listening on port 8000!')
+});
